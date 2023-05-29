e70c26b72f13729ad21d9b7a43b8fba7506be363
Neander de Souza <neander.faria@hotmail.com>
2023-05-29 16:26:39 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Neander de Souza',
+    email: 'mcspipoca47@gmail.com',
+  },
+})
