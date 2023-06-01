9fdad4bed5180f70980afbdfcabd2991d1295819
Neander de Souza <neander.faria@hotmail.com>
2023-06-01 14:49:54 -0300

    Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,11 +4,13 @@ import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 
+import { verifyJWT } from './middllwares/verify-jwt'
+
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
