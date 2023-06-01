a5ba730c2ed338332534b6b80ba861a46955e7a3
Neander de Souza <neander.faria@hotmail.com>
2023-06-01 10:10:06 -0300

    Repósitorio do Prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,18 @@
-import { Prisma, User } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 
 import { UsersRepository } from '../users-repository'
 
 import { prisma } from '@/lib/prisma'
 
 export class PrismaUsersRepository implements UsersRepository {
-  findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
 
   async findByEmail(email: string) {
