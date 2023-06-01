e1eba0e13b92789c5b26ae22bde5be48c7c4f3fb
Neander de Souza <neander.faria@hotmail.com>
2023-06-01 16:32:11 -0300

    Controllers das rotas de check-ins

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import fastify from 'fastify'
 import { ZodError } from 'zod'
 
 import { env } from './env'
-import { usersRoutes } from './http/controllers/users/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
+import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
@@ -14,6 +15,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -4,12 +4,12 @@ import { z } from 'zod'
 import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
 
 export async function search(request: FastifyRequest, reply: FastifyReply) {
-  const searchGymsBodySchema = z.object({
+  const searchGymsQuerySchema = z.object({
     q: z.string(),
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { q, page } = searchGymsBodySchema.parse(request.body)
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
 
   const searchGymsUseCase = makeSearchGymsUseCase()
 
